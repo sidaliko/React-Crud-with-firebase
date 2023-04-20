774754e25a37a21260b8895bac74759801586f11
sidaliko <58235791+sidaliko@users.noreply.github.com>
2023-04-20 13:43:09 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfwZqnbFSxSZQF6kXrb38DiPTep0vdV9A",
-  authDomain: "crud-firebase-react-47288.firebaseapp.com",
-  projectId: "crud-firebase-react-47288",
-  storageBucket: "crud-firebase-react-47288.appspot.com",
-  messagingSenderId: "681970979546",
-  appId: "1:681970979546:web:d29214eb377bd9cdc14be3",
-  measurementId: "G-VXVGR5Q63V",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
